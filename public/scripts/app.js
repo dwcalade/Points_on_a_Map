90ef2be241f7a18edeba7696f1b0034c401692eb
nicholasreimer <nchlsreimer@gmail.com>
2022-05-09 18:52:38 +0000

    added additional notes to files relating to leaflet, no code added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,11 @@
 // Client facing scripts here
+// this is where you are going to be asking the DB for
+// specific info and then using that info to make get requests
+// that will then go into leaflet
 
-$(() => {
+//all the functions related to leaflet actions will live here
+// fucntions that deletes a map, fucntion that populates a map etc
 
+//research tweeter, get requests, ajax, charcounter function
 
-
-})
+$(() => {});
--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//the backend server
+
 // load .env data into process.env
 require("dotenv").config();
 
