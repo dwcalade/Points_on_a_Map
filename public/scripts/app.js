24f2cfb7aafcc9f72b65e4f6b5c556d50eeedb92
dwcalade <dwcalade@yahoo.ca>
2022-05-11 18:32:19 +0000

    Finished QuickStart to Leaflet

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,12 +29,8 @@ $(() => {
     $("html, body").animate({ scrollTop: "9999" }, 1000);
   })
 
-// add leaflet here
-  // const map = L.map('mapid').setView([,], 12);
 
-  // L.tileLayer('', {
 
-  // }).addTo(map);
 
   const popupContent = `
     <form class="marker-form">
@@ -140,10 +136,24 @@ $(() => {
 
 //add a module exports underneath
 
-const express = require("express");
-const mapRoutes = express.Router();
+// const express = require("express");
+// const mapRoutes = express.Router();
 
-$(() => {});
+$(() => {
+
+// add leaflet here
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+ attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+ maxZoom: 18,
+ id: 'mapbox/streets-v11',
+ tileSize: 512,
+ zoomOffset: -1,
+ accessToken: 'pk.eyJ1Ijoibmljb2xhc3JlaW1lciIsImEiOiJjbDJ5NTZrdWUxMjR5M2xtdjBlOXd5cTlhIn0.aD_v3fkw1sd4Ps5jd1Bccg'
+}).addTo(map);
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
 
 // establish the database as per the erd before attempting to pull anything from it
 
@@ -156,3 +166,4 @@ function addMarker(click){
     .bindPopup(popupContent)
     .openPopup();
 }
+});
