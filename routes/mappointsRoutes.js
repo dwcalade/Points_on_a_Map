82a323cd809f756ac3ead7a1f2093c91b7b76789
nicholasreimer <nchlsreimer@gmail.com>
2022-05-11 00:08:53 +0000

    added a couple notes to mappointsRoutes and added new temporary routes to mapRoutes

--- routes/mappointsRoutes.js
+++ routes/mappointsRoutes.js
@@ -4,63 +4,46 @@ const express = require("express");
 const router = express.Router();
 module.exports = (db) => {
   //-------------------------------------------------------------------------------
-  // MAP POINTS RELATED SERVER ROUTES: (see google doc)
-  //-----------------------------
+  // MAP POINTS - SERVER ROUTES: (see google doc)
+  //-------------------------------------------------------------------------------
 
-  //CHANGE ALL BELOW FOLLOW TABLE DOC
-
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-      .then((data) => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-
-  router.get("/test2", (req, res) => {
-    db.query(`SELECT * FROM users;`)
-
-      .then((data) => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch((err) => {
-        res.status(500).json({ error: err.message });
-      });
-  });
-
-  //Add a new point to a list given its id
+  //POST: Add a new map point to a list given its id
   router.post("/:mapid/add", (req, response) => {
-  const ownerId = 1;//req.session.user.id;
-  const listId = req.params.listid;
-  const { title, description, imgUrl, latitude, longitude } = req.body;
+    const ownerId = 1; //req.session.user.id;
+    const listId = req.params.listid;
+    const { title, description, imgUrl, latitude, longitude } = req.body;
 
-  addPoint(db, ownerId, listId, title, description, imgUrl, latitude, longitude)
-    .then(() => response.status(201).send());
+    addPoint(
+      db,
+      ownerId,
+      listId,
+      title,
+      description,
+      imgUrl,
+      latitude,
+      longitude
+    ).then(() => response.status(201).send());
   });
 
-  //Update title/description of a point
+  //-------------------------------------------------------------------------------
+  //POST: Update title/description/image of a map point
   router.post("/:mapid/update/:pinid", (req, response) => {
-  const { pointid } = req.params;
-  const { title, description, imgUrl } = req.body;
+    const { pointid } = req.params;
+    const { title, description, imgUrl } = req.body;
 
-  updatePoint(db, pointid, title, description, imgUrl)
-    .then(() => response.status(201).send());
+    updatePoint(db, pointid, title, description, imgUrl).then(() =>
+      response.status(201).send()
+    );
   });
 
-   //Delete a point from a list
-   router.post("/:mapid/remove/:pinid", (req, response) => {
-
-    deletePoint(db, req.params.pinid)
-      .then(() => response.status(201).send());
-      res.redirect('/maps');
+  //-------------------------------------------------------------------------------
+  //Delete a point from a list
+  router.post("/:mapid/remove/:pinid", (req, response) => {
+    deletePoint(db, req.params.pinid).then(() => response.status(201).send());
+    res.redirect("/maps");
   });
 
+  //-------------------------------------------------------------------------------
   //connects this file with server.js
   return router;
 };
-
-//-------------------------------------------------------------------------------
--- routes/mapsRoutes.js
+++ routes/mapsRoutes.js
@@ -23,7 +23,7 @@ module.exports = (db) => {
   });
 
   //-------------------------------------------------------------------------------
-  //GET: /maps /:map_id - guest or user clicks a map in the map list box they get redirected to that specific map
+  //GET: /maps /:map_id - guest or user clicks a map in the map list box and are redirected to that map
 
   router.get("/:map_id", (req, res) => {
     db.query(`SELECT * FROM maps WHERE id = 1;`)
@@ -43,8 +43,7 @@ module.exports = (db) => {
   // it will create a new map_id for the database
 
   router.post("/create", (req, res) => {
-    // when the ejs form is built the value inputed by the client will be interpolated into the INSERT below
-    console.log("before the dbQuerry");
+    // when the ejs form is built the value inputed by the user will be interpolated into the INSERT below
     db.query(
       `INSERT INTO maps(name, latitude, longitude)
        VALUES ('mapsRoute.js TEST', 49.69695, -483.155365);`
@@ -61,6 +60,29 @@ module.exports = (db) => {
   });
 
   //-------------------------------------------------------------------------------
+  //POST: /maps /favorites - If a guest or user favorites a specific map that map gets saved to them.
+
+  router.post("/create", (req, res) => {
+    //conditional asks whether the client is a user or a guest
+    //if()
+
+    // when the ejs form is built the value inputed by the user will be interpolated into the INSERT below
+    db.query(
+      `INSERT INTO favorite_maps(user_id, map_id)
+       VALUES (1, 5);`
+    )
+      // -the .then runs when the above DB insert is successfull and redirects the client to the specific
+      //  map they were just on
+      .then((data) => {
+        return res.redirect("map/1");
+      })
+      .catch((err) => {
+        console.log("error: ", err);
+        res.status(500).json({ error: err.message });
+      });
+  });
+
+  //-------------------------------------------------------------------------------
   //connects this file with server.js
-  return router;
+  https: return router;
 };
