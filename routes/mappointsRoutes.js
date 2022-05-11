3bef8920cc8b4d7f3f832e15f42f523fae5febe3
Danny Jiang <dannyjiang11@outlook.com>
2022-05-11 22:49:21 +0000

    add routes

--- routes/mappointsRoutes.js
+++ routes/mappointsRoutes.js
@@ -7,34 +7,59 @@ module.exports = (db) => {
   // MAP POINTS - SERVER ROUTES: (see google doc)
   //-------------------------------------------------------------------------------
 
-  //POST: Add a new map point to a list given its id
-  router.post("/:mapid/add", (req, response) => {
-    const ownerId = 1; //req.session.user.id;
-    const listId = req.params.listid;
-    const { title, description, imgUrl, latitude, longitude } = req.body;
+  router.get("/:mapid", (req, res) => {
+    const id = req.params.id;
+    const userId = req.session.userID;
+    res.render('map_view', { id, userId});
+  });
 
-    //function still needs to be built
-    addPoint(title, description, imgUrl, latitude, longitude).then(() =>
-      response.status(201).send()
-    );
+  //POST: Add a new map point to a list given its id
+  router.post("/:mapid/add", (req, res) => {
+    const userId = req.session.user_id;
+    const id = req.params.id;
+    const title = req.body.title;
+    const description = req.body.description;
+    const image = req.body.image;
+    const lat = req.params.lat;
+    const lng = req.params.lat;
+
+    db.query(`SELECT user_id FROM maps
+    WHERE maps.id = $1`,
+    [req.params.id])
+      .then((data) => {
+        if ((data.rows[0].user_id) != userId) {
+
+          const errorMsg = 'You must be an authenticated user to add a point to a map!';
+          res.render('error', { id, userId, errorMsg});
+          return;
+
+        } else {
+
+          db.query(`INSERT INTO points (title, description, image, latitude, longitude, map_id)
+          VALUES ($1,$2,$3,$4,$5,$6)`,
+          [id, lng, lat, title, description, image]);
+
+          res.render('map_view', { id, userId });
+        }
+      });
   });
 
   //-------------------------------------------------------------------------------
   //POST: Update title/description/image of a map point
-  router.post("/:map_id/update/:pin_id", (req, response) => {
+  router.post("/:map_id/update/:pin_id", (req, res) => {
     const { pointid } = req.params;
     const { title, description, imgUrl } = req.body;
 
     //MISSING: DB Querry?
     updatePoint(db, pointid, title, description, imgUrl).then(() =>
-      response.status(201).send()
+      res.status(201).send()
     );
   });
 
   //-------------------------------------------------------------------------------
   //Delete a point from a list
-  router.post("/:map_id/remove/:pin_id", (req, response) => {
-    deletePoint(db, req.params.pinid).then(() => response.status(201).send());
+  router.post("/:map_id/remove/:pin_id", (req, res) => {
+    deletePoint(db, req.params.pinid).then(() => res.status(201).send());
     res.redirect("/maps");
   });
 
