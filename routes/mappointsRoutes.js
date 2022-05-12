ed62dfd2bb2c99a4e7d69e5023309acffa8c600a
nicholasreimer <nchlsreimer@gmail.com>
2022-05-12 17:57:13 +0000

    blank

--- routes/mappointsRoutes.js
+++ routes/mappointsRoutes.js
@@ -2,7 +2,7 @@
 // SERVER SETUP:
 const express = require("express");
 const router = express.Router();
-const pointsHelper = require('../views/Helper Functions/renderMapPoints');
+const pointsHelper = require("../views/Helper Functions/renderMapPoints");
 
 module.exports = (db) => {
   //-------------------------------------------------------------------------------
@@ -11,35 +11,33 @@ module.exports = (db) => {
 
   //POST: Add a new map point to a list given its id
 
-  router.post('/:mapid/add', (req, res) => {
+  router.post("/:mapid/add", (req, res) => {
     let pointsInfo = {
-            name: req.body.name,
-            description: req.body.description,
-            image: req.body.image,
-            latitude: Number(req.body.latitude),
-            longitude: Number(req.body.longitude),
-            map_id: Number(req.body.map_id)
-      }
-    pointsHelper.addPoints(db, pointsInfo).then(dbRes => res.json(dbRes));
+      name: req.body.name,
+      description: req.body.description,
+      image: req.body.image,
+      latitude: Number(req.body.latitude),
+      longitude: Number(req.body.longitude),
+      map_id: Number(req.body.map_id),
+    };
+    pointsHelper.addPoints(db, pointsInfo).then((dbRes) => res.json(dbRes));
   });
-
 
   //-------------------------------------------------------------------------------
   //POST: Update title/description/image of a map point
   router.post("/:map_id/update/:pin_id", (req, res) => {
     const pointId = req.body.pointId;
-    pointsHelper.editPoint(db, pointId).then()
+    pointsHelper.editPoint(db, pointId).then();
   });
 
   //-------------------------------------------------------------------------------
   //Delete a point from a list
   router.post("/:map_id/remove/:pin_id", (req, res) => {
     const pointId = req.body.pointId;
-    pointsHelper.deletePoint(db, pointId).then(() =>  res.send('ok'));
+    pointsHelper.deletePoint(db, pointId).then(() => res.send("ok"));
   });
 
   //-------------------------------------------------------------------------------
   //connects this file with server.js
   return router;
 };
-
